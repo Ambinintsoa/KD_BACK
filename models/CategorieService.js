4a4f257131fc2a5ba25e4726d247ca3993749846
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-11 23:48:27 +0300

    Create model CategorieService

---
+++ models/CategorieService.js
@@ -0,0 +1,6 @@
+const mongoose=require("mongoose");
+
+const CategorieServiceSchema=new mongoose.Schema({
+    nom_categorie:{type:String,required:true},
+});
+module.exports=mongoose.model('CategorieService',CategorieServiceSchema);

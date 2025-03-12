149ea9fb92428ef2cbde6fa5ce89a4aeb1d3c2cc
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-12 22:48:10 +0300

    fix: fix the type of field that should be Number instead of int,double

--- models/Produit.js
+++ models/Produit.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const ProduitSchema = new mongoose.Schema({
     nom_produit: { type: String, required: true },
-    prix: { type: double, required: false },
+    prix: { type: Number, required: false },
     unite: { type: String, required: true }
 }, {
     timestamps: true // Option de timestamp pour createdAt et updatedAt

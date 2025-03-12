7671373586ae02bc18b7d7c2924f5f91d4461615
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-12 22:26:53 +0300

    create model DetailsDevis

---
+++ models/DetailsDevis.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DetailsDevisSchema = new mongoose.Schema({
+    prix: { type: double, required: true },
+    devis: { type: mongoose.Schema.Types.ObjectId, ref: "Devis", required: true },
+    service: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true  },
+    usage_produit_service: { type: mongoose.Schema.Types.ObjectId, ref: "Usage_Produit_Service", required: false }
+}, {
+  timestamps: true // Option de timestamp pour createdAt et updatedAt
+});
+module.exports = mongoose.model('DetailsDevis',DetailsDevisSchema);

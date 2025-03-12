202ce8a4438485b38b18a7616b8fcafd0adc7aed
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-12 22:27:42 +0300

    refactor: rename Usage_Produit_Service to UsageProduitService

--- models/UsageProduitService.js
+++ models/UsageProduitService.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const Usage_Produit_ServiceSchema = new mongoose.Schema({
+const UsageProduitServiceSchema = new mongoose.Schema({
     service: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true },
     produit: { type: mongoose.Schema.Types.ObjectId, ref: "Produit", required: true },
     quantite: { type: double, required: true }
 }, {
     timestamps: true // Option de timestamp pour createdAt et updatedAt
 });
-module.exports = mongoose.model('Usage_Produit_Service', Usage_Produit_ServiceSchema);
+module.exports = mongoose.model('UsageProduitService', UsageProduitServiceSchema);

a430a887007cca553e730b16f86ea1ab1ea2389f
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-12 22:02:48 +0300

    create model Voiture

---
+++ models/Voiture.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const VoitureSchema = new mongoose.Schema({
+    immatriculation: { type: String, required: true },
+    kilometrage: { type: double, required: false },
+    marque: { type: String, required: true },
+    modele: { type: String, required: true },
+    client: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },
+}, {
+    timestamps: true // Option de timestamp pour createdAt et updatedAt
+});
+module.exports = mongoose.model('Voiture', VoitureSchema);

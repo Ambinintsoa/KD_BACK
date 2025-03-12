03936249d32417ecde29ac2f3c8f4b9bc64b0e32
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-12 22:06:53 +0300

    create model Devis

--- models/CategorieService.js
+++ models/CategorieService.js
@@ -1,6 +1,6 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const CategorieServiceSchema=new mongoose.Schema({
-    nom_categorie:{type:String,required:true},
+const CategorieServiceSchema = new mongoose.Schema({
+    nom_categorie: { type: String, required: true },
 });
-module.exports=mongoose.model('CategorieService',CategorieServiceSchema);
+module.exports = mongoose.model('CategorieService', CategorieServiceSchema);
--- models/Service.js
+++ models/Service.js
@@ -1,11 +1,11 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const ServiceSchema=new mongoose.Schema({
-    nom_service:{type:String,required:true},
-    duree:{type:int,required:false},
-    prix:{type:double,required:false},
-    categorie_service:{type:mongoose.Schema.Types.ObjectId,ref:"CategorieService",required:true}
-},{
-    timestamps: true // Option de timestamp pour createdAt et updatedAt
-  });
-module.exports=mongoose.model('Service',ServiceSchema);
+const ServiceSchema = new mongoose.Schema({
+  nom_service: { type: String, required: true },
+  duree: { type: int, required: false },
+  prix: { type: double, required: false },
+  categorie_service: { type: mongoose.Schema.Types.ObjectId, ref: "CategorieService", required: true }
+}, {
+  timestamps: true // Option de timestamp pour createdAt et updatedAt
+});
+module.exports = mongoose.model('Service', ServiceSchema);

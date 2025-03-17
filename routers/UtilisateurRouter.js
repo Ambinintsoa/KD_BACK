f8e1a7f5e9e4ea5a001a22599c1255a825acb992
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-17 22:41:29 +0300

    feat: add the links for the features

--- routers/UtilisateurRouter.js
+++ routers/UtilisateurRouter.js
@@ -1,8 +1,11 @@
-const express=require("express");
-const userRouter=express.Router();
-
-const UtilisateurController=require("../controllers/UtilisateurController");
+const express = require("express");
+const userRouter = express.Router();
+const verifyToken = require('../middlewares/AuthMiddleware');
+const UtilisateurController = require("../controllers/UtilisateurController");
 
 userRouter.post("/register", UtilisateurController.register);
+userRouter.post("/login", UtilisateurController.login);
+userRouter.get("/refresh",UtilisateurController.refreshToken);
+userRouter.get("/:page",verifyToken,UtilisateurController.read);
 
 module.exports = userRouter;

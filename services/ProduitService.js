bed5803e8cb3647c6f6d3f05dd6e2d479531cc8a
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-03-19 20:36:05 +0300

    fix: add some control in the attributes of some models

--- services/ProduitService.js
+++ services/ProduitService.js
@@ -54,8 +54,9 @@ exports.update = async(data)=>{
         const initial_produit = await Produit.findOne({ _id:produit._id });
         if(! initial_produit) throw new Error("Aucun produit correspondant !");
         
-        initial_produit.nom_produit =produit.nom_produit.trim(); // Mise à jour de l'attribut
-        initial_produit.unite=produit.unite.trim();
+        initial_produit.nom_produit = (produit.nom_produit && produit.nom_produit.trim()) || initial_produit.nom_produit; // Mise à jour de l'attribut
+        initial_produit.unite = (produit.unite && produit.unite.trim()) || initial_produit.unite; // Mise à jour de l'attribut
+        
         await initial_produit.save(); // Sauvegarde les modifications
     } catch (error) {
         console.error(error);

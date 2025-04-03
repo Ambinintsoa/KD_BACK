d2d33b7c2173c188629ebecab058781fe7748b8a
Fy Andrianina <118877328+Fy-Andrianina@users.noreply.github.com>
2025-04-03 19:15:55 +0300

    add attribute rendez_vous in the Facture model

--- models/Facture.js
+++ models/Facture.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const FactureSchema = new mongoose.Schema({
     date: { type: Date, required: true },
     client: { type: mongoose.Schema.Types.ObjectId, ref: "Utilisateur", required: true },
+    rendez_vous:{type: mongoose.Schema.Types.ObjectId, ref: "RendezVous", required:false },
     montant_total: { type: Number, required: false },
     statut: { type: String, required: true },
     numero_facture:{ type: String, required: true },

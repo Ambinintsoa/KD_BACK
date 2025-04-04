432e3f89e34a919c955df056e1457fe8532c28e3
isabelle <irandriambololona@ingenosya.mg>
2025-04-04 12:54:48 +0300

    [FEAT]facture liste

--- routers/FactureRouter.js
+++ routers/FactureRouter.js
@@ -4,10 +4,11 @@ const FactureController=require("../controllers/FactureController");
 const verifyToken = require('../middlewares/AuthMiddleware');
 const RoleMiddleware = require('../middlewares/RoleMiddleware');
 
-factureRouter.get("/factures_client",[verifyToken], FactureController.readByClient);
-factureRouter.get("/facture_rdv/:rdv",[verifyToken], FactureController.readByScoreDESC);
-factureRouter.get("/detail_facture/:facture",[verifyToken], FactureController.readById);
-
+//factureRouter.get("/factures_client",[verifyToken], FactureController.readByClient);
+//factureRouter.get("/facture_rdv/:rdv",[verifyToken], FactureController.readByScoreDESC);
+//factureRouter.get("/detail_facture/:facture",[verifyToken], FactureController.readById);
+factureRouter.get('/', [verifyToken,RoleMiddleware.adminRole], FactureController.getFactures);
+factureRouter.get('/client', [verifyToken], FactureController.getFacturesByClient);
 
 module.exports = factureRouter;
 

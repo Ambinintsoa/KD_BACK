b54d5de76c64155b58a3c3707cb53186c0e8d87a
Fy-Andrianina <meeranhailee@gmail.com>
2025-04-04 21:24:40 +0300

    fix: change the update tache function

--- models/Tache.js
+++ models/Tache.js
@@ -4,7 +4,7 @@ const TacheSchema = new mongoose.Schema({
   service: { type: mongoose.Schema.Types.ObjectId, ref: "Service", required: true },
   duree: { type: Number, required: false },
   ordre_priorite: { type: Number, required: false },
-  statut: { type: String, required: false,default:"A faire" },
+  statut: { type: Number, required: false,default:0 },
   rendez_vous: { type: mongoose.Schema.Types.ObjectId, ref: "RendezVous", required: true }
 }, {
   timestamps: true // Option de timestamp pour createdAt et updatedAt
